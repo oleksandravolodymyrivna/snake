8686bd06bd13b6f24a983285dd503c23f13520f7
Oleksandra <oleksandra.chernyak@gmail.com>
2015-12-01 19:01:23 +0100

    Funkcja Draw()

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,8 +10,24 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Sasza");
+            int x1 = 1; //координаты
+            int y1 = 3; //координаты
+            char sym1 = '*'; //создаем знак звездочка
+            Draw(x1, y1, sym1);
+
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '#';
+            Draw(x2, y2, sym2);
+
             Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+
         }
     }
 }
